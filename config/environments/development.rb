79ee3cc7a85e68157f3e08f138695f77e5a066c8
Viktoria Dounar-Schabowska <viktoria.dounar@gmail.com>
2018-08-15 14:11:18 -0700

    mail setup

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,8 +16,6 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
@@ -40,4 +38,16 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  ActionMailer::Base_smtp_settings = {
+    address:        'smtp.gmail.com',
+    port:           '25',
+    authentication: :plain,
+    user_name:      'vika.blocipedia@gmail.com',
+    password:       'BlocAdminDupa8!',
+    domain:         'gmail.com',
+    enable_starttls_auto: true
+  }
 end
