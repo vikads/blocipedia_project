53ce6c063e8984622dd15fe64b0d392f755dfc31
Viktoria Dounar-Schabowska <viktoria.dounar@gmail.com>
2018-08-20 18:50:28 -0700

    wiki controller

---
+++ app/controllers/wikis_controller.rb
@@ -0,0 +1,30 @@
+class WikisController < ApplicationController
+  def index
+    @wikis = Wiki.all
+  end
+
+  def show
+  end
+
+  def new
+    @wiki = Wiki.new
+  end
+
+  def create
+    @wiki = Wiki.new
+    @wiki.title = params[:wiki][:title]
+    @wiki.body = params[:wiki][:body]
+
+    if @wiki.save
+
+      flash[:notice] = "Wiki was saved."
+      redirect_to @wiki
+    else
+      flash.now[:alert] = "There was an error saving the wiki. Please try again."
+      render :new
+    end
+  end
+
+  def edit
+  end
+end
