2ac49485dc51f39436d0450dd7cb75e5f5e8801e
Viktoria Dounar-Schabowska <viktoria.dounar@gmail.com>
2018-08-30 18:43:44 -0700

    seeding data

--- app/controllers/wikis_controller.rb
+++ app/controllers/wikis_controller.rb
@@ -3,7 +3,8 @@ class WikisController < ApplicationController
 
   def index
     @user = User.find_by(id: session[:user_id])
-    @wikis = policy_scope(Wiki)
+    # @wikis = policy_scope(Wiki)
+    @wikis = Wiki.all
   end
 
   def show
