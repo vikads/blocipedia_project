af1b01440afb52c457f8186785b1ce1be619bb99
Viktoria Dounar-Schabowska <viktoria.dounar@gmail.com>
2018-09-05 13:25:59 -0700

    creating charges

---
+++ app/controllers/charges_controller.rb
@@ -0,0 +1,40 @@
+class ChargesController < ApplicationController
+
+  def create
+  # Creates a Stripe Customer object, for associating
+  # with the charge
+  customer = Stripe::Customer.create(
+    email: current_user.email,
+    card: params[:stripeToken]
+  )
+
+  # Where the real magic happens
+  charge = Stripe::Charge.create(
+    customer: customer.id, #Note -- this is NOT the uer_id in the app
+    amoutn: 15_00,
+    description: "Premium Membership - #{current_user.email}",
+    currency: 'usd'
+  )
+
+
+  flash[:notice] = "Thanks for all the money, #{current_user.email}! Now you are a PREMIUM member!."
+  current_user.update_attribute(:role, 'premium')
+  redirect_to rooth_path # or wherever
+
+
+
+  # Stripe will send back CardErrors, with friendly messages
+  # when something goes wrong.
+  # This `rescue block` catches and displays those errors.
+  rescue Stripe::CardError => e
+    flash[:alert] = e.message
+    redirect_to new_charge_path
+end
+
+def new
+  @stripe_btn_data = {
+    key: "#{Rails.configurations.stripe[:publishable_key] }",
+    description: "Premium Membership - #{current_user.name}",
+    amount: 15_00
+  }
+end 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   resources :wikis
+  resources :charges, only [:new, :create]
 
   devise_for :users
 
