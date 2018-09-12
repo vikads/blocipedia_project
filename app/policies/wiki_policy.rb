f8693b7d5db219dae180864b3c0fdd583368f74f
Viktoria Dounar-Schabowska <viktoria.dounar@gmail.com>
2018-09-12 11:25:18 -0700

    collaborators model

--- app/policies/wiki_policy.rb
+++ app/policies/wiki_policy.rb
@@ -42,29 +42,34 @@ class WikiPolicy < ApplicationPolicy
     def resolve
       wikis = []
       # if user.admin?
-      #   scope.all
+      #   wikis =  scope.all # if the user is an admin, show them all the wikis
+      # elsif user.nil?
+      #   wikis = scope.where(private: false)
       if user.nil?
         wikis = scope.where(private: false)
+      elsif user.admin?
+        wikis =  scope.all # if the user is an admin, show them all the wikis
+
       elsif user.premium?
         all_wikis = scope.all
         all_wikis.each do |wiki|
-          if !wiki.private || user = wiki.user || user.admin?
-            wikis << wiki
+          if wiki.public? || wiki.owner == user || wiki.collaborators.include?(user)
+            wikis << wiki # if the user is premium, only show them public wikis, or that private wikis they created, or private wikis they are a collaborator on
           end
         end
-      else
+      else #this is the lowly standard user
         all_wikis = scope.all
         wikis = []
         all_wikis.each do |wiki|
-          if !wiki.private || user.admin? || user == wiki.user
-            wikis<<wiki
+          if !wiki.private || wiki.collaborators.include?(user)
+            wikis<<wiki # only show standard users public wikis and private wikis they are a collaborator on
           end
         end
       end
-      wikis
-      end
+      wikis # return the wikis array we've built up
     end
-  # end
+  end
+
 
   def is_authorized?(user, record)
     !record.private || user.admin? || user == record.user
